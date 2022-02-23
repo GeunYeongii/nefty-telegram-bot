331bcfcf05b107b8f5be86511d184693e043773a
taranndus <taranndus@gmail.com>
2022-02-23 13:59:55 +0900

    nftgo수정

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pycoingecko import CoinGeckoAPI
 import os
 from WhaleTracker.nftgo import nftgoAPI
 
@@ -13,7 +12,8 @@ import logging
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console..
 
-APITOKEN = '5101309356:AAHowCuy5dMrisjkGivPXionz4-xBzalXSs'
+
+APITOKEN = os.environ.get('APITOKEN') 
 bot = telebot.TeleBot(APITOKEN)
 
 def gen_markup():
@@ -37,8 +37,7 @@ def callback_query(call):
         buyers = nft.get_buyer_list()
         images = nft.get_nft_image()
         
-        for name,price,change,buyer,image in zip(names,prices,changes,buyers,images) :
-             
+        for name,price,change,buyer,image in zip(names,prices,changes,buyers,images) :            
             # nft 정보를 HTML형식으로 메세지 보내기
             bot.send_message(call.from_user.id,"<b>이름 :</b> {0}\n<b>{1} :</b> {2}\n<b>{3} : </b> {4}원\n<b>변동률 :</b> {5}%\n<b>구매자 :</b> <a href='https://opensea.io/{6}'>🔗Opensea Link</a>\n<b>NFT :</b> <a href='{7}'> ▶ 이미지 보러가기</a>".format(name,price['ETH'][1],price['ETH'][0],price['KRW'][1],format(price['KRW'][0],','),change['percent'],buyer,image),parse_mode="HTML")
     except:
